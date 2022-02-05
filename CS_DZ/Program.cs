7b2ce747804e41fd75dffdd763c12db07344d7e0
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-05 16:25:21 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int picturesInLine = 3;
+            int fullLines = allPictures / picturesInLine;
+            int remainePictures = allPictures - fullLines * picturesInLine;
+
+            Console.WriteLine("Количество заполненых рядов- " + fullLines + " Оставшиеся картинки- " + remainePictures);
+        }
+    }
+}
